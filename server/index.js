5ac8dbc9730769cfd1f68febdeb2e9f2f4037201
Nayanika-12 <nayanikasc2002@gmail.com>
2025-08-27 00:19:23 +0530

    pushed the code for readme

--- server/index.js
+++ server/index.js
@@ -4,14 +4,14 @@ const fileUpload = require("express-fileupload");
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const clothesRoutes = require("./routes/clothes");
 const outfitsRoutes = require("./routes/outfits");
 const path = require("path");
 // const staticPath = path.join(__dirname, "/pictures");
 
 app.use(cors());
-app.use(fileUpload()); //default filesize is 1mb
+app.use(fileUpload({ limits: { fileSize: 10 * 1024 * 1024 } })); //default filesize is 1mb
 // max size for the request body is 1mb
 app.use(express.json({ limit: "1mb" }));
 // server static files in the public folder
